e341108ee8ec452632e00327c14852ca667609bb
MrFormiga1212 <lisbo.h2o@gmail.com>
2024-05-05 22:47:50 -0300

    Domingo noite #02

--- aps_rpg/src/aps_rpg/Fase_1.java
+++ aps_rpg/src/aps_rpg/Fase_1.java
@@ -1,5 +1,47 @@
 package aps_rpg;
 
-public class Fase_1 {
+import aps_rpg_Textos.Texto_Introdução;
 
+public class Fase_1 {
+	private Texto_Introdução textoIntroducao;
+	
+	public Fase_1(String nick) {
+		textoIntroducao = new Texto_Introdução(nick);
+	}
+	
+	public void iniciarTexto01() {
+		System.out.println("Fase 1 - Jornada pelo Rio Tietê");
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt01());
+	}
+	
+	public void iniciarTexto02() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt02());
+	}
+	
+	public void iniciarTexto03() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt03());
+	}
+	
+	public void iniciarTexto04() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt04());
+	}
+	
+	public void iniciarTexto05() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt05());
+	}
+	
+	public void iniciarTexto06() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt06());
+	}
+	
+	public void iniciarTexto07() {
+		System.out.println("------------------------------");
+		System.out.println(textoIntroducao.getTxt07());
+	}
 }
