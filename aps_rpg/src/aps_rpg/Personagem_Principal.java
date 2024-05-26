11b67030fc3c6518c56e9fdd3af21e170c63c7f5
Cristyan Lisboa <77303268+Cristyan14@users.noreply.github.com>
2024-05-25 22:40:50 -0300

    Update Personagem_Principal.java

--- aps_rpg/src/aps_rpg/Personagem_Principal.java
+++ aps_rpg/src/aps_rpg/Personagem_Principal.java
@@ -8,47 +8,47 @@ public class Personagem_Principal {
     private int vida;
     private int maxVida;
     
-    public Personagem_Principal(String nome) {
+    public Personagem_Principal(String nome) { // Método Construtor
         this.nome = nome;
         this.ataque = 7 + new Random().nextInt(9); // Gera um número entre 7 e 15
         this.vida = 80;
         this.setMaxVida(80);
     }
     
-    public String getNome() {
+    public String getNome() { // Encapsulamento
 		return nome;
 	}
 
 
-	public void setNome(String nome) {
+	public void setNome(String nome) { // Encapsulamento
 		this.nome = nome;
 	}
 
 
-	public int getAtaque() {
+	public int getAtaque() { // Encapsulamento
 		return ataque;
 	}
 
 
-	public void setAtaque(int ataque) {
+	public void setAtaque(int ataque) { // Encapsulamento
 		this.ataque = ataque;
 	}
 
 
-	public int getVida() {
+	public int getVida() { // Encapsulamento
 		return vida;
 	}
 
 
-	public void setVida(int vida) {
-		this.vida = vida;
+	public void setVida(int vida) { // Encapsulamento
+		this.vida = vida; 
 	}
 	
-	public int getMaxVida() {
+	public int getMaxVida() { // Encapsulamento
 		return maxVida;
 	}
 
-	public void setMaxVida(int maxVida) {
+	public void setMaxVida(int maxVida) { // Encapsulamento
 		this.maxVida = maxVida;
 	}
 
